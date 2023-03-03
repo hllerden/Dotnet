3cbf614a1081191a552d637d001a1f41da67aaed
hllerden <halilxerden@gmail.com>
2023-03-03 11:58:45 +0300

    just a commit

--- Hello/HelloWebapi/Controllers/WeatherForecastController.cs
+++ Hello/HelloWebapi/Controllers/WeatherForecastController.cs
@@ -41,17 +41,4 @@ public class WeatherForecastController : ControllerBase
         })
         .ToArray();
     }
-/*
-    //api/WeatherFroecasts?id=3
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> GetById([FromQuery]string id)
-    {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
-    }*/
 }
